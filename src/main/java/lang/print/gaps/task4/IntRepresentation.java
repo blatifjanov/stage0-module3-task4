b5f16b23f964f1ff86fd4a321e54382f08a16e0a
Bakhovuddin Latifjanov <salokhiddin2016@Bakhovuddins-MacBook-Pro.local>
2023-02-20 23:03:20 +0500

    Primitives

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,12 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int first=99, second=104,third=97,fourth=114;
+        char a = (char) first,b = (char)second, c=(char) third, d=(char)fourth;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
     }
 }
